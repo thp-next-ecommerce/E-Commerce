4318bee53123874facf6a03ff5e630bb89986c46
christopheleray <christophe.leray1@gmail.com>
2019-01-23 17:10:27 +0100

    add confid route for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  mount Sidekiq::Web, at: "/sidekiq"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
