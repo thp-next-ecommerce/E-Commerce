d97dcba9c85718928e06ae987e85f931af3aaaad
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-24 12:02:18 +0100

    user is confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'static_pages#home'
   get 'contact', to: 'static_pages#contact'
   get 'about', to: 'static_pages#about'
