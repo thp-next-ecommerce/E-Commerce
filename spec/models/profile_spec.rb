4fcff9722efde366fc3c789804039ad1dcf907ca
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-24 16:17:15 +0100

    add database test

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  it { is_expected.to have_db_column(:first_name).of_type(:string) }
-  it { is_expected.to have_db_column(:last_name).of_type(:string) }
-  it { is_expected.to have_db_column(:address).of_type(:string) }
-  it { is_expected.to have_db_column(:phone).of_type(:string) }
-  it { is_expected.to have_db_column(:birth_date).of_type(:date) }
-  it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
-  it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  describe 'Database' do
+    it { is_expected.to have_db_column(:first_name).of_type(:string) }
+    it { is_expected.to have_db_column(:last_name).of_type(:string) }
+    it { is_expected.to have_db_column(:address).of_type(:string) }
+    it { is_expected.to have_db_column(:phone).of_type(:string) }
+    it { is_expected.to have_db_column(:birth_date).of_type(:date) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
 end
