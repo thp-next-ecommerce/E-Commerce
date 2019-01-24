5abf17358314815a5b793362e1191fd01a7aff9d
christopheleray <christophe.leray1@gmail.com>
2019-01-24 15:31:33 +0100

    add conf mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+                                        address: 'localhost',
+                                        port: 1030,
+                                        domain: 'localhost'
+                                      }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
