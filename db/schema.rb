2e05310daa806ecc5c9669acb40c70037eeb0c68
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-25 14:04:55 +0100

    delete address attribut to profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_163623) do
+ActiveRecord::Schema.define(version: 2019_01_25_130026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_01_24_163623) do
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "address"
     t.string "phone"
     t.date "birth_date"
     t.datetime "created_at", null: false
