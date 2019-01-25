8cbcffa99a2e185bd740ef4f8be2672f4021e85a
Tim <timothee.bull@gmail.com>
2019-01-25 14:28:14 +0100

    added edition model and associations

---
+++ db/migrate/20190125132713_create_editions.rb
@@ -0,0 +1,10 @@
+class CreateEditions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :editions do |t|
+      t.string :name
+      t.datetime :release_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/editions.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :edition do
+    name { "MyString" }
+    release_date { "2019-01-25 14:27:13" }
+  end
+end
