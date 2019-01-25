d62d429e56e6ec6270a07b603089f0be9db5a089
Ronan BORGNE <ronan.borgne@laposte.net>
2019-01-25 18:15:26 +0100

    test address + annotate

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: orders
+#
+#  id              :bigint(8)        not null, primary key
+#  subtotal        :decimal(12, 2)
+#  tax             :decimal(12, 2)
+#  shipping        :decimal(12, 2)
+#  total           :decimal(12, 2)
+#  order_status_id :bigint(8)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+
 FactoryBot.define do
   factory :order do
     subtotal { "9.99" }
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: orders
+#
+#  id              :bigint(8)        not null, primary key
+#  subtotal        :decimal(12, 2)
+#  tax             :decimal(12, 2)
+#  shipping        :decimal(12, 2)
+#  total           :decimal(12, 2)
+#  order_status_id :bigint(8)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
