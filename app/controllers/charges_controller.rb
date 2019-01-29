ad921e0784e0142cb8db7c10e3d8c74784979db6
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-29 18:37:21 +0100

    stripe settings

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -15,7 +15,7 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       customer: customer.id,
       amount: @amount,
-      description: 'Pack of 5 cards',
+      description: 'Magic payment',
       currency: 'usd'
     )
   rescue Stripe::CardError => e
