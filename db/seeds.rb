1eca7af353e4a28c2a5bc147743c6d0cc42bdd21
Tim <timothee.bull@gmail.com>
2019-01-29 10:46:33 +0100

    order_spec done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Edition.delete_all
-Edition.create! id: 1, name: "Edition 1"
-Card.delete_all
-Card.create! id: 1, name: "test 1", original_price: 0.49, active: true, edition_id: 1
-Card.create! id: 2, name: "test 2", original_price: 0.29, active: true, edition_id: 1
-Card.create! id: 3, name: "test 3", original_price: 1.99, active: true, edition_id: 1
-
-OrderStatus.delete_all
-OrderStatus.create! id: 1, name: "In Progress"
-OrderStatus.create! id: 2, name: "Placed"
-OrderStatus.create! id: 3, name: "Shipped"
-OrderStatus.create! id: 4, name: "Cancelled"
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -17,32 +17,31 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  subject { build(:order) }
+  subject(:order) { create(:order) }
 
-  describe 'database and association' do
+  describe 'database' do
     it { is_expected.to have_db_column(:subtotal).of_type(:decimal) }
     it { is_expected.to have_db_column(:tax).of_type(:decimal) }
     it { is_expected.to have_db_column(:shipping).of_type(:decimal) }
     it { is_expected.to have_db_column(:total).of_type(:decimal) }
     it { is_expected.to have_db_column(:order_status_id).of_type(:integer) }
+  end
+
+  describe 'association' do
     it { is_expected.to belong_to(:order_status) }
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:order_items) }
+
+    it 'follows a relational link through order_status model up to itself' do
+      expect(order.order_status.orders.first).to be_truthy
+    end
+
+    it 'follows a relational link through user model up to itself' do
+      expect(order.user.orders.first).to be_truthy
+    end
   end
 
-  # describe 'assocociation' do
-  #   it 'follows a relational link through order_status model up to itself' do
-  #     order = create(:order).reload
-  #     expect(order.order_status.orders.first).to eq order
-  #   end
-  #   it 'follows a relational link through user model up to itself' do
-  #     order = create(:order).reload
-  #     expect(order.user.orders.first).to eq order
-  #   end
-  # end
-
-  # it 'is creatable' do
-  #   order = create(:order).reload
-  #   expect(order.id).not_to be_nil
-  # end
+  it 'is creatable' do
+    expect(order.id).not_to be_nil
+  end
 end
