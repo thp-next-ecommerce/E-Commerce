4a1bf33af1e375f3981f66b03f90df19215323b6
christopheleray <christophe.leray1@gmail.com>
2019-01-30 11:51:03 +0100

    update factories items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -20,7 +20,7 @@ FactoryBot.define do
     name { "MyString" }
     description { "MyText" }
     has_discount { false }
-    original_price { 1.5 }
-    discounted_percentage { 1 }
+    original_price { 100 }
+    discounted_percentage { 50 }
   end
 end
