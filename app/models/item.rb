1fc482541bf5c795b05f11f698563461863c5366
christopheleray <christophe.leray1@gmail.com>
2019-01-30 18:10:59 +0100

    add scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,12 +19,18 @@ class Item < ApplicationRecord
   has_one :stock, dependent: :destroy
   has_many :order_items, dependent: :destroy
 
-  default_scope { where(active: true) }
   validates :name, presence: true, length: { minimum: 3 }
-  validates :original_price, presence: true, numericality: true, allow_nil: false
+  validates :original_price, presence: true, allow_nil: false
   validates :has_discount, default: false
   validates :discounted_percentage,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+            numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+            
+  default_scope { where(active: true) }
+  scope :sorted_by_price, -> { order("original_price ASC") }
+  scope :sorted_by_name, lambda { order("name ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
+  scope :with_discount, lambda { where(has_discount: true) }
+  scope :without_discount, lambda { where(has_discount: false) }
 
   def price
     has_discount ? (original_price.to_i * (1 - discounted_percentage.to_f / 100)).round(2) : original_price.to_i
