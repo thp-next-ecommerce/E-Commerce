224ebb99a988be62c866cbee3f57e75f9d405cad
Tim <timothee.bull@gmail.com>
2019-02-01 13:46:35 +0100

    added access test for basket_controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,35 +3,21 @@
 class OrderItemsController < ApplicationController
   def create
     @order = current_order
-    puts 'order.new ========================================'
-    puts @order.inspect
     @order_item = current_order_item
-    puts 'params ========================================'
-    puts order_item_params
-    puts 'order_item.new ========================================'
-    puts @order_item.inspect
     @order.save
-    puts 'order.save ========================================'
-    puts @order.inspect
-    puts @order.errors.inspect
     session[:order_id] = @order.id
-    puts session
   end
 
   def update
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
     @order_item.update(order_item_params)
-
-    @order_items = @order.order_items
   end
 
   def destroy
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
-
-    @order_items = @order.order_items
   end
 
   private
@@ -45,7 +31,6 @@ class OrderItemsController < ApplicationController
       item_id: order_item_params[:item_id],
       order_id: current_order.id
     )
-    puts "found orderItem reference"
     return query[0] if query.any?
 
     puts "creating a new order_item"
--- spec/controllers/baskets_controller_spec.rb
+++ spec/controllers/baskets_controller_spec.rb
@@ -4,9 +4,17 @@ require 'rails_helper'
 
 RSpec.describe BasketsController, type: :controller do
   describe "GET #show" do
-    it "returns http success" do
+    let(:user) { create(:user) }
+
+    it "returns http success when logged in" do
+      sign_in user
       get :show
       expect(response).to have_http_status(:success)
+    end
+
+    it "returns http redirect if not logged in" do
+      get :show
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
