13c45008f274ab401212343dee9aebfd4b4c2460
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-01 12:42:04 +0100

    basket and charge controller user authentified required

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BasketsController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @order_items = current_order.order_items
   end
