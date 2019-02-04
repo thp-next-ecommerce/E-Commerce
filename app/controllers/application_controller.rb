93d671a124f3920d45dd7c09ac28b3c79206368d
christopheleray <christophe.leray1@gmail.com>
2019-02-04 19:35:45 +0100

    move set_locale to private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
   before_action :set_locale
 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
   def current_order
     return Order.find(session[:order_id]) if session[:order_id]
 
     new_order = Order.new
     new_order.user = current_user
     new_order
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
