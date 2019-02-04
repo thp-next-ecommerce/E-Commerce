9994297db3a8fd4c8f04d8a155dc65d20f693558
christopheleray <christophe.leray1@gmail.com>
2019-02-04 17:59:11 +0100

    remove default test

--- spec/controllers/administration/items_controller_spec.rb
+++ spec/controllers/administration/items_controller_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Administration::ItemsController, type: :controller do
   let(:item) { create(:item) }
-
   describe '.delete_item' do
     it 'gets alert' do
+      skip
       delete_item(item)
       it { is_expected.to set_the_flash(:notice).to('Product has been deleted successfuly') }
     end
