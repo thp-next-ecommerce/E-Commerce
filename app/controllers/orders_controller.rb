09739ea0a7a6e67c9f01655c6602abc49b861062
christopheleray <christophe.leray1@gmail.com>
2019-02-04 16:41:56 +0100

    remove puts comments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,7 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    puts "-------"
-    puts @order.inspect
-    puts "-------"
     @order_items = OrderItem.where(order_id: @order.id)
-    puts @order_items.inspect
-    puts "-------"
   end
 
   def index
