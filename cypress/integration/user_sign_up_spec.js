24292d7fb18c2ad96c2e350b5f579427d90820ea
System Administrator <root@netrestore-a8206613d6e1.local>
2019-02-05 12:39:29 +0100

    add admin sign in test

--- cypress/integration/user_sign_up_spec.js
+++ cypress/integration/user_sign_up_spec.js
@@ -1,11 +1,11 @@
 describe('User sign_up test', function() {
-  it('A visitor can create an account', function() {
+  it('A visitor can create a user account', function() {
     cy.visit('https://e-commerce-stagings.herokuapp.com/')
 
     cy.contains("S'inscrire").click()
     cy.get('#user_email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
+      .type('alexis@gmail.com')
+      .should('have.value', 'alexis@gmail.com')
     cy.get('#user_password')
       .type('azerty123')
       .should('have.value', 'azerty123')
