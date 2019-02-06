9980ee3d643b62f5631feebb7406029496694d8e
Ronan BORGNE <borgne.ronan@gmail.com>
2019-02-06 21:48:20 +0100

    error pagination résolu

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   # Shop
   resources :charges, only: %i[new create]
   resources :items, only: %i[index show] do
-    get ':page', action: :index, on: :collection
+    get 'page/:page', action: :index, on: :collection
   end
   resource :basket, only: [:show]
   resources :order_items, only: %i[create update destroy]
